0f2a375edd47cddff0e73b7dfa95567d717aade3
Sean <sd.cotter45@gmail.com>
2021-11-02 16:45:58 -0400

    heroku

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -1,70 +1,70 @@
-import { createContext, useContext, useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+// import { createContext, useContext, useEffect, useRef } from "react";
+// import { useSelector } from "react-redux";
 
 
-export const SocketContext = createContext(null);
-export const Socket = () => useContext(SocketContext);
+// export const SocketContext = createContext(null);
+// export const Socket = () => useContext(SocketContext);
 
-function SocketProvider(props) {
-    const user = useSelector(state => state.session.user);
+// function SocketProvider(props) {
+//     const user = useSelector(state => state.session.user);
 
-    const socket = useRef(null);
+//     const socket = useRef(null);
 
-    useEffect(() => {
-        if (!user) return;
-        if (socket.current) return;
+//     useEffect(() => {
+//         if (!user) return;
+//         if (socket.current) return;
 
-        const websocket = new WebSocket(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_WS_URL : 'wss://localhost:5000');
+//         const websocket = new WebSocket(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_WS_URL : 'wss://localhost:5000');
 
-        websocket.onopen = (e) => {
-            console.log('Socket Connection');
-        }
+//         websocket.onopen = (e) => {
+//             console.log('Socket Connection');
+//         }
 
-        websocket.onerror = (e) => {
-            console.error(e);
-        }
+//         websocket.onerror = (e) => {
+//             console.error(e);
+//         }
         
-        websocket.onclose = (e) => {
-            console.log('socket close')
-            socket.current = null;
-        }
+//         websocket.onclose = (e) => {
+//             console.log('socket close')
+//             socket.current = null;
+//         }
         
-        socket.current = websocket;
+//         socket.current = websocket;
         
-        // return function cleanup() {
-        //     console.log('cleanup')
-        //     if (socket.current !== null) {
-        //         socket.current.close();
-        //     }
-        // }
-    }, [user])
+//         // return function cleanup() {
+//         //     console.log('cleanup')
+//         //     if (socket.current !== null) {
+//         //         socket.current.close();
+//         //     }
+//         // }
+//     }, [user])
 
-    // useEffect(() => {
-    //     if (websocket.current !== null) {
-    //         websocket.current.onmessage = (e) => {
-    //             const parse = JSON.parse(e.data);
-    //             const message = parse.data;
-    //             message.created = new Date(message.created);
+//     // useEffect(() => {
+//     //     if (websocket.current !== null) {
+//     //         websocket.current.onmessage = (e) => {
+//     //             const parse = JSON.parse(e.data);
+//     //             const message = parse.data;
+//     //             message.created = new Date(message.created);
                 
-    //             if (parse.type === 'add-chat-message') {
-    //                 setMessages([message, ...messages]);
-    //             } 
-    //             else if (parse.type === 'user-drawing' && message.username !== username) {
-    //                 setUserDraw([message, ...userDraw])
-    //             }
-    //             else if (parse.type === 'user-reset') {
-    //                 setReset(!reset);
-    //             }
-    //         }
-    //     }
+//     //             if (parse.type === 'add-chat-message') {
+//     //                 setMessages([message, ...messages]);
+//     //             } 
+//     //             else if (parse.type === 'user-drawing' && message.username !== username) {
+//     //                 setUserDraw([message, ...userDraw])
+//     //             }
+//     //             else if (parse.type === 'user-reset') {
+//     //                 setReset(!reset);
+//     //             }
+//     //         }
+//     //     }
 
-    // }, [messages, reset, setMessages, userDraw, username, websocket])
+//     // }, [messages, reset, setMessages, userDraw, username, websocket])
     
-    return (
-        <SocketContext.Provider value={socket}>
-            {props.children}
-        </SocketContext.Provider>
-    )
-}
+//     return (
+//         <SocketContext.Provider value={socket}>
+//             {props.children}
+//         </SocketContext.Provider>
+//     )
+// }
 
-export default SocketProvider;
+// export default SocketProvider;
