e3cb5976ccb3df5cadc5138041678735ec026150
Sean <sd.cotter45@gmail.com>
2021-11-04 22:43:32 -0400

    got drag n drop working for tasks, going to edit table to save positions

--- frontend/src/components/UserProfile/to-dos/task.js
+++ frontend/src/components/UserProfile/to-dos/task.js
@@ -1,27 +1,94 @@
 import { useSpring, animated } from 'react-spring';
-import { useDrag } from '@use-gesture/react';
+// import { useDrag } from '@use-gesture/react';
+import { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 
 
-function Task({ task }) {
+function Task({ task, moveTask, moveTask2, moveTask3, index }) {
 
     const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
-    const parent = document.querySelector(`.priority-${task.priority}`);
-    const parentBounds = parent.getBoundingClientRect();
-    console.log(parentBounds);
+    const ref = useRef();
 
-//     const bind = useDrag(({ down, offset: [ox, oy] }) => api.start({ x: ox, y: oy, immediate: down }), {
-//     bounds: parentBounds,
-//   })
+    const type = 'task';
 
-    // bind drag to parent container bounds
-    const bind = useDrag(({ offset: [x, y] }) => api.set({ x, y, }), {
-        bounds: parentBounds,
-    })
+    const [{ isDragging }, drag] = useDrag({
+        type,
+        item: { type, id: task.id, index, priority: task.priority },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
 
+    const [, drop] = useDrop({
+        accept: type,
+        hover(item, monitor) {
+            if (!ref.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+            const hoverBoundary = ref.current.getBoundingClientRect();
+            const hoverMiddleY = (hoverBoundary.bottom - hoverBoundary.top) / 2;
+            const hoverActualY = monitor.getClientOffset().y - hoverBoundary.top;
+
+            if (dragIndex < hoverIndex && hoverActualY < hoverMiddleY) return
+            if (dragIndex > hoverIndex && hoverActualY > hoverMiddleY) return
+
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+
+            moveTask(dragIndex, hoverIndex);
+            item.index = hoverIndex;
+        }
+    });
+
+    const [, drop2] = useDrop({
+        accept: type,
+        hover(item) {
+            if (!ref.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+
+            moveTask2(dragIndex, hoverIndex);
+            item.index = hoverIndex;
+        }
+    });
+
+    const [, drop3] = useDrop({
+        accept: type,
+        hover(item) {
+            if (!ref.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+
+            moveTask3(dragIndex, hoverIndex);
+            item.index = hoverIndex;
+        }
+    });
+
+    // drag(drop(drop2(drop3(ref))));
+    drag(drop(ref));
 
     return (
-        <animated.div {...bind()} style={{x, y}} className="task-container">
+        <animated.div draggable ref={ref} style={{x, y, opacity: isDragging ? 0 : 1 }} className="task-container">
             <h3>{task.title}</h3>
+            <div className='bar'></div>
+            <div className='emptybar'></div>
+            <div className='filledbar'></div>
+            <p id={task.priority === 1 ? 'high' : task.priority === 2 ? 'medium' : 'low'} >{task.priority === 1 ? 'High Priority' : task.priority === 2 ? 'Medium Priority' : 'Low Priority'}</p>
             <p>{new Date(task.createdAt).toLocaleTimeString()} {new Date(task.createdAt).toLocaleDateString()}</p>
             <p>{task.description}</p>
         </animated.div>
--- frontend/src/components/UserProfile/to-dos/todo.js
+++ frontend/src/components/UserProfile/to-dos/todo.js
@@ -1,14 +1,19 @@
-import { useState, useEffect, Suspense } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Loading from '../../../util/loading';
+import { DndProvider, DropTarget } from 'react-dnd';
+import { findDOMNode } from 'react-dom';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import update from 'immutability-helper';
 
+import Loading from '../../../util/loading';
 import Task from "./task";
 import './to-do.css';
+
 
 function ToDo({ tasks }) {
     const userId = useParams().userId;
 
-    
+    const [taskCards, setTasks] = useState([]);
     const [priority1, setPriority1] = useState(tasks?.filter(task => task.priority === 1));
     const [priority2, setPriority2] = useState(tasks?.filter(task => task.priority === 2));
     const [priority3, setPriority3] = useState(tasks?.filter(task => task.priority === 3));
@@ -17,30 +22,99 @@ function ToDo({ tasks }) {
         setPriority1(tasks?.filter(task => task.priority === 1));
         setPriority2(tasks?.filter(task => task.priority === 2));
         setPriority3(tasks?.filter(task => task.priority === 3));
+        setTasks(tasks);
     }, [tasks]);
 
-    console.log(priority1)
+
+    const moveTask = (dragIndex, hoverIndex) => {
+        const dragCard = taskCards[dragIndex];
+        setTasks(update(taskCards, {
+            $splice: [
+                [dragIndex, 1],
+                [hoverIndex, 0, dragCard],
+            ],
+        }));
+    };
+
+    const moveTask2 = (dragIndex, hoverIndex) => {
+        const dragCard = priority2[dragIndex];
+        setPriority2(
+            update(priority2, {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, dragCard],
+                ],
+            }),
+        );
+    };
+
+    const moveTask3 = (dragIndex, hoverIndex) => {
+        const dragCard = priority3[dragIndex];
+        setPriority3(
+            update(priority3, {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, dragCard],
+                ],
+            }),
+        );
+    };
+    
+
     return (
         <>
         {!tasks && <Loading />}
         {tasks && (
-            <div className='todo-container'>
-                <div className="todo">
-                    {/* <h1>TO DO APP HERE</h1> */}
-                    <p>High Priority</p>
-                    <div className='priority-1 priority-container'>
-                        {priority1 && priority1.map(task => <Task key={task.id} task={task} />)}
-                    </div>
-                    <p>Medium Priority</p>
-                    <div className='priority-2 priority-container'>
-                        {priority2 && priority2.map(task => <Task key={task.id} task={task} />)}
-                    </div>
-                    <p>Low Priority</p>
-                    <div className='priority-3 priority-container'>
-                        {priority3 && priority3.map(task => <Task key={task.id} task={task} />)}
+            <DndProvider backend={HTML5Backend}>
+                <div className='todo-container'>
+                    <div className="todo">
+                        {taskCards && taskCards.map((task, index) => (
+                            <Task 
+                                key={task.id}
+                                index={index}
+                                task={task}
+                                moveTask={moveTask}
+                                moveTask2={moveTask2}
+                                moveTask3={moveTask3}
+                            />
+                        ))}
+                        {/* <h1>TO DO APP HERE</h1> */}
+                        {/* <p>High Priority</p>
+                        <div className='priority-1 priority-container'>
+                            {priority1 && priority1.map((task, index) => <Task
+                                moveTask={moveTask}
+                                // moveTask2={moveTask2}
+                                // moveTask3={moveTask3}
+                                key={task.id} 
+                                task={task} 
+                                index={index}
+                             />)}
+                        </div>
+                        <p>Medium Priority</p>
+                        <div className='priority-2 priority-container'>
+                            {priority2 && priority2.map((task, index) => <Task
+                                // moveTask={moveTask}
+                                moveTask2={moveTask2}
+                                // moveTask3={moveTask3}
+                                key={task.id} 
+                                task={task} 
+                                index={index}
+                             />)}
+                        </div>
+                        <p>Low Priority</p>
+                        <div className='priority-3 priority-container'>
+                            {priority3 && priority3.map((task, index) => <Task
+                                // moveTask={moveTask}
+                                // moveTask2={moveTask2}
+                                moveTask3={moveTask3}
+                                key={task.id} 
+                                task={task} 
+                                index={index}
+                             />)}
+                        </div> */}
                     </div>
                 </div>
-            </div>
+            </DndProvider>
         )}
         </>
     )
