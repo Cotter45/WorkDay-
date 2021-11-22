31f765ba67e77ec163f1745802b8968e9683c562
Sean <sd.cotter45@gmail.com>
2021-11-22 15:48:07 -0500

    added a preview tag because im going to push to heroku despite unfinished crud because of the ux improvements

--- frontend/src/components/UserProfile/to-dos/todo.js
+++ frontend/src/components/UserProfile/to-dos/todo.js
@@ -110,6 +110,7 @@ function ToDo() {
         {!loaded && <Loading />}
         {loaded && (
             <div className='todo-container'>
+                <h2>Preview, not yet finished</h2>
                 <div className='todo-heading'>
                     <h2>Drag and drop tasks to mark complete</h2>
                     <button className='add-task-button' onClick={() => console.log('new task')}>+</button>
