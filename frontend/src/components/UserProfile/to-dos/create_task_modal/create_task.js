d289a3bb39a4797626ccc1bfd99799dfb3a2aa2b
Sean <sd.cotter45@gmail.com>
2021-11-24 12:43:48 -0500

    error handling for bad urls completed

--- frontend/src/components/UserProfile/to-dos/create_task_modal/create_task.js
+++ frontend/src/components/UserProfile/to-dos/create_task_modal/create_task.js
@@ -19,7 +19,12 @@ function CreateTask({ tasks }) {
     })
 
     const handleURL = (e) => {
-        setUrl(e.target.value);
+        if (!url.includes('http') || !url.includes('https')) {
+            setErrors([...errors, { image: 'URL is not valid' } ])
+        } else {
+            setImages([...images, url]);
+            setUrl('');
+        }
     }
 
     const handleSubmit = (e) => {
@@ -49,30 +54,39 @@ function CreateTask({ tasks }) {
                     <div className='task_images'>
                         <label>Images</label>
                         <div className='edit-buttons'>
-                        <label>URL</label>
-                        <input
-                            value={url}
-                            onChange={(e) => handleURL}
-                            className='post-image-input'
-                        ></input>
-                        <label>OR</label>
-                        <label className='upload'>
+                            <label>URL</label>
                             <input
-                                type='file'
-                                className='upload-button'
-                                onChange={(e) => e.target.files[0] ? setImages([...images, e.target.files[0]]) : null} 
-                                ></input>
-                            Choose File
-                        </label>
-                    </div>
-                    <div className='task_images_preview'>
-                        {images.map((image, index) => (
+                                value={url}
+                                onChange={(e) => setUrl(e.target.value)}
+                                onFocus={() => setErrors([])}
+                                className='post-image-input'
+                            ></input>
+                            {!url && (
+                                <>
+                                <label>OR</label>
+                                <label className='upload'>
+                                    <input
+                                        type='file'
+                                        className='upload-button'
+                                        onChange={(e) => e.target.files[0] ? setImages([...images, e.target.files[0]]) : null} 
+                                        ></input>
+                                    Choose File
+                                </label>
+                                </>
+                            )}
+                            {url && (
+                                <button className='submit' onClick={handleURL}>Add</button>
+                            )}
+                        </div>
+                        {errors.map((err, index) => err.image && <p key={index} className='error'>{err.image}</p>)}
+                        <div className='task_images_preview'>
+                            {images.map((image, index) => (
                                 <div className='task_image' key={index}>
                                     <button onClick={() => setImages(images.filter((_image, _index) => _index !== index))}>X</button>
-                                    <img src={URL.createObjectURL(image)} alt='task_image' />
+                                    <img src={image?.name ? URL.createObjectURL(image) : image} alt='task_image' />
                                 </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
                     </div>      
                 </div>
             </form>
