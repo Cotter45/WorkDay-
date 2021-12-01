60aff9f5212339031be259192131ba9cb2cb77b8
Sean <sd.cotter45@gmail.com>
2021-12-01 15:32:44 -0500

    starting to add css for info modal for tasks

--- frontend/src/components/UserProfile/to-dos/task-modal/task-details.js
+++ frontend/src/components/UserProfile/to-dos/task-modal/task-details.js
@@ -8,14 +8,16 @@ function TaskDetails({ task, setShowModal }) {
 
     return (
         <div className="task-details">
-            <h1>details</h1>
-            <h3><div id={task.priority === 1 ? 'high' : task.priority === 2 ? 'medium' : 'low'} ></div>{task.title}</h3>
+            <div className='task-header'>
+                <div id={task.priority === 1 ? 'high' : task.priority === 2 ? 'medium' : 'low'} ></div>
+                <h3>{task.title}</h3>
+            </div>
             <div className='bar'></div>
             <div className='emptybar'></div>
             <div className='filledbar'></div>
-            <p className='task-date'>{new Date(task.createdAt).toLocaleTimeString()} {new Date(task.createdAt).toLocaleDateString()}</p>
+            <p className='task-date'><strong>Created at:</strong> {new Date(task.createdAt).toLocaleTimeString()} {new Date(task.createdAt).toLocaleDateString()}</p>
             <p>---</p>
-            <p className='task-description'>{task.description}</p>
+            <p className='task-description'><strong>Description: </strong>{task.description}</p>
         </div>
     )
 }
