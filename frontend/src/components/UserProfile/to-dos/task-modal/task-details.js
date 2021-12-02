39331d755ff75d1c0050900f64ae20e9b5ce157c
Sean <sd.cotter45@gmail.com>
2021-12-02 11:28:14 -0500

    working on in place update for task details

--- frontend/src/components/UserProfile/to-dos/task-modal/task-details.js
+++ frontend/src/components/UserProfile/to-dos/task-modal/task-details.js
@@ -1,24 +1,59 @@
 import { useSpring } from 'react-spring';
+import { useState, useEffect } from 'react';
 
 import './task_details.css';
 
 function TaskDetails({ task, setShowModal }) {
 
+    const [update, setUpdate] = useState(false);
+    const [title, setTitle] = useState(task.title);
+    const [priority, setPriority] = useState(task.priority);
+
     console.log(task)
 
     return (
-        <div className="task-details">
-            <div className='task-header'>
-                <div id={task.priority === 1 ? 'high' : task.priority === 2 ? 'medium' : 'low'} ></div>
-                <h3>{task.title}</h3>
+        <>
+        {!update && (
+            <div className="task-details">
+                <div className='task-buttons'>
+                    <button onClick={() => setUpdate(!update)}><i className='fas fa-edit'></i></button>
+                    <button onClick={() => setShowModal(false)}><i className='fas fa-times'></i></button>
+                </div>
+                <div className='task-header'>
+                    <div id={priority === 1 ? 'high' : priority === 2 ? 'medium' : 'low'} ></div>
+                    <h3>{title}</h3>
+                </div>
+                <div className='bar'></div>
+                <div className='emptybar'></div>
+                <div className='filledbar'></div>
+                <p className='task-date'><strong>Created at:</strong> {new Date(task.createdAt).toLocaleTimeString()} {new Date(task.createdAt).toLocaleDateString()}</p>
+                <p>---</p>
+                <p className='task-description'><strong>Description: </strong>{task.description}</p>
             </div>
-            <div className='bar'></div>
-            <div className='emptybar'></div>
-            <div className='filledbar'></div>
-            <p className='task-date'><strong>Created at:</strong> {new Date(task.createdAt).toLocaleTimeString()} {new Date(task.createdAt).toLocaleDateString()}</p>
-            <p>---</p>
-            <p className='task-description'><strong>Description: </strong>{task.description}</p>
-        </div>
+        )}
+        {update && (
+            <div className="task-details">
+                <div className='task-buttons'>
+                    <button onClick={() => setUpdate(!update)}><i className='fas fa-check'></i></button>
+                    <button onClick={() => setShowModal(false)}><i className='fas fa-times'></i></button>
+                </div>
+                <div className='task-header'>
+                    <div id={priority === 1 ? 'high' : priority === 2 ? 'medium' : 'low'} ></div>
+                    {/* <h3>{task.title}</h3> */}
+                    <input 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className='bar'></div>
+                <div className='emptybar'></div>
+                <div className='filledbar'></div>
+                <p className='task-date'><strong>Created at:</strong> {new Date(task.createdAt).toLocaleTimeString()} {new Date(task.createdAt).toLocaleDateString()}</p>
+                <p>---</p>
+                <p className='task-description'><strong>Description: </strong>{task.description}</p>
+            </div>
+        )}
+        </>
     )
 }
 
